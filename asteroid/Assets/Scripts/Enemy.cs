ec277ca8ba202275d0f1da88224366dc1da8ce4d
ISAIAH SANCHEZ <ISASANCH@uat.edu>
2018-05-30 13:35:39 -0700

    Asteroid
    Added GameManager, Player, Enemy

---
+++ asteroid/Assets/Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	public float health;
+	public float moveSpeed;
+	public float damage;
+
+	// Use this for initialization
+	void Start () 
+	{
+		GameManager.instance.enemies.Add (this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnDestroy()
+	{
+		GameManager.instance.enemies.Remove (this);
+	}	
+}
---
+++ asteroid/Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float moveSpeed;
+	public float turnSpeed;
+	public float health;
+
+	// Use this for initialization
+	void Start () 
+	{
+		GameManager.instance.player = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
